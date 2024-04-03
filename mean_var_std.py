ba0d1b1323f154370e2e183f9042287de979bc81
Kor Pog <kor.pogorzelczyk@gmail.com>
2024-04-03 18:17:22 +0000

    fisrt commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def calculate(list):
-
-
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    arr = np.array([list[0:3]], [list[3:6], list[6:9]])
+    print(arr)
+    means = [arr.me]
+    calculations = {}
 
 
     return calculations
